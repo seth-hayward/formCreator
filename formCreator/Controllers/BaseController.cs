74929795d5c99bbe028867fd14deee452d7f381e
Seth Hayward <seth.hayward@gmail.com>
2012-12-07 17:24:20 -0500

    Added recommended BaseController used by AppHabor -- best practice?

---
+++ formCreator/Controllers/BaseController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Raven.Client;
+
+// base controller used from RavenDBTest sample code provided by AppHarbor
+// https://github.com/friism/RavenDBMvcSample/blob/master/RavenDBTest/Controllers/BaseController.cs
+namespace formCreator.Controllers
+{
+    public abstract class BaseController : Controller
+    {
+        public IDocumentSession RavenSession { get; private set; }
+
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            RavenSession = MvcApplication.documentStore.OpenSession();
+        }
+
+        protected override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            if (filterContext.IsChildAction)
+                return;
+
+            using (RavenSession)
+            {
+                if (filterContext.Exception != null)
+                    return;
+
+                if (RavenSession != null)
+                    RavenSession.SaveChanges();
+            }
+        }
+    }
+}
