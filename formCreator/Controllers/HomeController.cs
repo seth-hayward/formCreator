70982ecf4effb864e7e16620b1ce771dd142fbed
Seth Hayward <seth.hayward@gmail.com>
2012-12-07 17:25:02 -0500

    Initial HomeController to display a count of Forms in system

--- formCreator/Controllers/HomeController.cs
+++ formCreator/Controllers/HomeController.cs
@@ -1,16 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using formCreator.Models;
 
 namespace formCreator.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "formCreator";
+
+            // load the character back we added in a previous Test Saving call
+            var characters = RavenSession.Query<Form>();
+
+            ViewBag.Message = String.Format("Loaded {0} Custom Forms", characters.Count());
 
             return View();
         }
