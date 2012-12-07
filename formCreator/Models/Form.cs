1aa75183d449ef5b4c353bdb13ec65ec20e15aac
Seth Hayward <seth.hayward@gmail.com>
2012-12-07 17:24:41 -0500

    Initial Form model

---
+++ formCreator/Models/Form.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace formCreator.Models
+{
+    public class Form
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class FormDBContext : DbContext
+    {
+        public DbSet<Form> Forms { get; set; }
+    }
+}
