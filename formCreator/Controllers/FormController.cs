9f01acd7d4db7ee1f26e996d2a378458ef61f2c2
Seth Hayward <seth.hayward@gmail.com>
2012-12-09 16:22:36 -0500

    Saving and Loading attributes on the Edit page

--- formCreator/Controllers/FormController.cs
+++ formCreator/Controllers/FormController.cs
@@ -87,6 +87,31 @@ namespace formCreator.Controllers
         public ActionResult Edit(int id)
         {
             Form form = (Form)RavenSession.Query<Form>().Where(f => f.Id == id).First();
+            String form_fields = null;
+
+            // interesting... the attributes list might actually
+            // be null here as well. how do ravendb developers deal
+            // with a changing model over time? and gracefully
+            // handle old models? they must have to update all
+            // old models in the database instead of relying on
+            // null checks.
+
+            if (form.Attributes == null)
+            {
+                form.Attributes = new List<Models.Attribute>();
+                RavenSession.SaveChanges();
+            }
+            else
+            {
+                foreach (Models.Attribute little_att in form.Attributes)
+                {
+                    form_fields += little_att.Name + ",";
+                }
+
+                form_fields = form_fields.TrimEnd(',');
+                ViewData["form_fields"] = form_fields;
+            }
+
             return View(form);
         }
 
@@ -98,12 +123,29 @@ namespace formCreator.Controllers
         {
             if (ModelState.IsValid)
             {
-
                 Form form_to_edit = (Form)RavenSession.Query<Form>().Where(f => f.Id == form.Id).First();
                 form_to_edit.Name = form.Name;
-                form_to_edit.Attributes = form.Attributes;
+
+                // this i am not very certain about...
+                // do we just clear the existing list of attributes?
+                // or do we loop through the form elements, check to 
+                // see if one exists, if no, add it -- if it does,
+                // check to see if the value is the same -- etc.
+                // i think this will look a lot cleaner when i am using
+                // ajax to handle individual form element updates instead
+                // of the whole shebang.
+
+                List<Models.Attribute> attributes = new List<Models.Attribute>();
+                String given_atts = Request["formAttributes"];
+                string[] atts = given_atts.Split(',');
+                foreach (string little_att in atts)
+                {
+                    attributes.Add(new Models.Attribute { Name = little_att, Value = little_att });
+                }
+
+                form_to_edit.Attributes = attributes;
                 RavenSession.SaveChanges();
-                return RedirectToAction("Index");
+                return RedirectToAction("Details", form);
             }
             return View(form);
         }
